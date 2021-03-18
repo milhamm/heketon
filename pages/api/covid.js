60b0bd234dd24c04e9a8a4c0e132d52d531ce9a7
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-03-18 16:03:14 +0700

    add api

--- pages/api/covid.js
+++ pages/api/covid.js
@@ -1,10 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import axios from 'axios';
+import NextCors from 'nextjs-cors';
 
-export default (req, res) => {
-  axios.get('https://api.kawalcorona.com/indonesia').then((data) => {
-    res.statusCode = 200;
-    res.json(data.data);
+export default async (req, res) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
+
+  const data = await axios.get('https://api.kawalcorona.com/indonesia');
+  res.statusCode = 200;
+  res.json(data.data);
+  res.end();
 };
